76c39e50493846ad633086d594f3a04806ab0a9e
Paulo Abner <pauloabner@gmail.com>
2016-07-16 18:51:58 +0000

    Encoding UTF-8

--- lib/clear_html.rb
+++ lib/clear_html.rb
@@ -1,10 +1,10 @@
 require 'nokogiri'
+#ClearHtml
 class ClearHtml
-
   def self.clear(content)
-    doc = File.open(content) { |f| Nokogiri::HTML(f) }
+    doc = File.open(content) { |f| Nokogiri::HTML(f, nil, 'UTF-8') }
     html = doc.to_html
-    html = remove_str(html,"&nbsp;")
+    html = remove_str(html, '&nbsp;')
     html = remove_attr(html, 'align')
     html = remove_br_clear_all(html)
     html = remove_attr(html, 'class')
@@ -18,38 +18,39 @@ class ClearHtml
     html
   end
 
-  private
+  private_class_method
+
   def self.remove_str(html, str)
     html.gsub(str, '')
   end
 
   def self.remove_attr(document, attr)
-    html = Nokogiri::HTML(document)
+    html = Nokogiri::HTML(document, nil, 'UTF-8')
     html.css('*').remove_attr(attr)
     html.to_html
   end
 
   def self.remove_br_clear_all(document)
-    html = Nokogiri::HTML(document)
+    html = Nokogiri::HTML(document, nil, 'UTF-8')
     html.css('br').each { |br| br.remove if br['clear'] == 'all' }
     html.to_html
   end
 
   def self.remove_empty_p(document)
-    html = Nokogiri::HTML(document)
+    html = Nokogiri::HTML(document, nil, 'UTF-8')
     html.css('p').each { |p| p.remove if p.text == "\u00A0" }
     html.to_html
   end
 
   def self.remove_tag(document, tag)
-    html = Nokogiri::HTML(document)
+    html = Nokogiri::HTML(document, nil, 'UTF-8')
     html.css(tag).remove
     html.to_html
   end
 
   def self.strip_tag(content, tag)
-    html = Nokogiri::HTML(content)
-    html.css(tag).each { |tag| tag.swap(tag.children) }
+    html = Nokogiri::HTML(content, nil, 'UTF-8')
+    html.css(tag).each { |t| t.swap(tag.children) }
     html.to_html
   end
 end
