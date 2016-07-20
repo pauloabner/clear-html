229b28036421f5aa5530fc91338b051c51ee2a0f
Paulo Abner Aurelio Mesquita <abner@atlantico.com.br>
2016-07-20 17:00:58 -0300

    apply_regexp_library

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,10 @@ class HomeController < ApplicationController
 
   def upload
     file = params['html_file']
-    @content = ClearHtml.clear file.path
-    @content.encode!('UTF-16', 'UTF-8', invalid: :replace, replace: '')
-    @content.encode!('UTF-8', 'UTF-16')
+    content = ClearHtml.clear file.path
+    content.encode!('UTF-16', 'UTF-8', invalid: :replace, replace: '')
+    content.encode!('UTF-8', 'UTF-16')
+    @content = apply_regexp_library content
   end
 
   def split
@@ -19,5 +20,11 @@ class HomeController < ApplicationController
 
   def split_content(content)
     content.split('<p><!-- pagebreak --></p>')
+  end
+
+  def apply_regexp_library content
+    file = File.read('public/regexp.txt')
+    data_hash = JSON.parse(file)
+    content
   end
 end
