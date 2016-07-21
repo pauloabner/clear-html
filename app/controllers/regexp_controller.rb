deeee8ccaa41b8174eca2d0d870e97d0fdf863c2
Paulo Abner Aurelio Mesquita <abner@atlantico.com.br>
2016-07-21 08:02:52 -0300

    Saving regexp

--- app/controllers/regexp_controller.rb
+++ app/controllers/regexp_controller.rb
@@ -6,6 +6,11 @@ class RegexpController < ApplicationController
   end
 
   def update
+    f = File.open('public/regexp.txt', 'w')
+    f.write(params['content'])
+    f.close
+    redirect_to regexp_url
+  rescue
     redirect_to regexp_url
   end
 end
